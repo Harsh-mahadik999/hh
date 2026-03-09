4f2835134a2d38a3a4a3175a69f5465d684000ca
Harsh Mahadik <mahadikhh@gmail.com>
2026-03-09 23:08:11 +0530

    Delete oo directory

--- oo/server/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { studentLogin, adminLogin } from '../controllers/authController';
-
-const router = Router();
-
-router.post('/student-login', studentLogin);
-router.post('/admin-login', adminLogin);
-
-export default router;
